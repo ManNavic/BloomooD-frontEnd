b1f78fbf477f1e20fddcedd843d5546b198aedad
Mantas Navickis <navickis@gmail.com>
2023-08-23 18:29:38 +0200

    index pageupdated

---
+++ src/pages/index/index.js
@@ -0,0 +1,11 @@
+import Header from '/Users/mantas/Desktop/Final-full-stack-project/BloomooD-frontEnd/src/components/header/index.js'
+import Slider from '/Users/mantas/Desktop/Final-full-stack-project/BloomooD-frontEnd/src/components/slider/slider.js'
+const Index = () => {
+return(
+    <>
+    <Header />
+    <Slider />
+    </>
+)
+}
+export default Index
