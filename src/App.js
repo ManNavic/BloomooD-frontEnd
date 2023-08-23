1edff14bc91f04811d0a3c59b069bdc3e49561d8
Mantas Navickis <navickis@gmail.com>
2023-08-23 18:27:30 +0200

    updated app.js routes and some css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from './pages/index/index';
+import Register from './pages/register/index';
+import Login from './pages/login/index';
 import './App.css';
+import Profile from "./pages/profile";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-main">
-        
-      </header>
+        <header className="App-main">
+        </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
