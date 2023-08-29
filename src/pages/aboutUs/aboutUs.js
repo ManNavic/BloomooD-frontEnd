5ee15b9a20d35a82a3b12c8dbdd0cd2514744570
Mantas Navickis <navickis@gmail.com>
2023-08-29 02:05:58 +0200

    created avout us component

---
+++ src/pages/aboutUs/aboutUs.js
@@ -0,0 +1,29 @@
+import '../aboutUs/aboutUs.css'
+const AboutUS = () => {
+return (
+    <div className="aboutUs-main" id='aboutUs'>
+<div className='welcome'>
+    <h3 className='welcome-title'>
+Welcome to Bloomood!</h3>
+<p className='welcome-content'>
+At Bloomood, we are passionate about bringing the beauty and tranquility of nature into your life through our exquisite collection of plants. Whether you're an experienced gardener or just starting to discover the joys of plant care, we're here to provide you with the highest quality plants, expert advice, and exceptional service.
+
+
+</p>
+</div>
+<div className='story'>
+    <h3 className='story-title'>Our Story</h3>
+<p className='story-content'>
+Founded in 2023, Bloomood sprouted from a deep love for plants and a desire to share that passion with others. What began as a small backyard endeavor has blossomed into a thriving plant community that serves plant enthusiasts across the country. Our journey has been marked by nurturing plants, cultivating relationships, and fostering a green environment.
+</p>
+</div>
+<div className='mission'>
+    <h3 className='mission-title'>Our Mission</h3>
+<p className='mission-content'>
+Our mission is simple yet profound: to inspire and empower people to cultivate their connection with nature. We believe that plants have the power to transform spaces and lives, improving air quality, enhancing well-being, and beautifying surroundings. Through our thoughtfully curated selection of plants, we aim to help you create lush indoor jungles, serene garden oases, and everything in between
+</p>
+</div>
+    </div>
+)
+}
+export default AboutUS
